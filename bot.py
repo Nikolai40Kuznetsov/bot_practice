ba9b8363624ab5a85d12393e4c241e652d2ff018
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-09-02 11:40:54 +0300

    eeeeeee

--- bot.py
+++ bot.py
@@ -1,6 +1,15 @@
 from aiogram import Bot, types
-from aiogram.dispatcher import dispatcher
+from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 import id
+
 TOKEN = id.name
 bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands = ['start', 'help'])
+async def send_welcome(msg: types.Message):
+    await msg.reply_to_message(f"Привет {msg.from_user.first_name}")
+
+if __name__ == "__main__":
+    dp.start_polling()
