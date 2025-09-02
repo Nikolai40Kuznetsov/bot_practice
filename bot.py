184c1de0b74f5abb0070c5370bc9569bcc894bde
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-09-02 11:27:03 +0300

    REFFE

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-import requests
-import aiogram
-import telegram
-import telebot
+from aiogram import Bot, types
+from aiogram.dispatcher import dispatcher
+from aiogram.utils import executor
+import id
+TOKEN = id.name
+bot = Bot(TOKEN)
