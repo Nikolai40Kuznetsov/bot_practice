1427119710c310caf23b72344c559bfa32eeb797
Nikolai40Kuznetsov <nikolaikuznetsov@tutamail.com>
2025-09-03 15:06:23 +0300

    gtdtdtdtdtdtdh

--- aiobot.py
+++ aiobot.py
@@ -3,22 +3,36 @@ from aiogram import Bot, types, Dispatcher
 from aiogram.filters import Command
 from id import name
 
-
 TOKEN = name
 bot = Bot(token = TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Привет! Я простой бот на aiogram")
+async def send_welcome(message: types.Message):
+    keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    info = types.KeyboardButton("Инфо")
+    stat = types.KeyboardButton("Статистика")
+    keyboard_markup.add(info, stat)
+    await message.answer("Привет! Я простой бот на aiogram")    
 
 @dp.message()
-async def echo(message: types.Message):
-    await message.answer(f"Ты написал {message.text}")
+async def echo(message: types.Sticker):
+    if message.text == "Инфо":
+        await message.answer(message, f"Ты нажал кнопку 'Инфо'")
+    elif message.text == "Статистика":
+        await message.answer(message, f"Ты нажал кнопку 'Статистика'")
+        # bot.send_sticker(message, ':love:')
+    else:
+        await message.answer(f"Ты написал {message.text}")
+        await message.answer(message, f"ID твоего стикера: {message.sticker.file_id}", parse_mode="Markdown")
 
 async def main():
-    await dp.start_polling(bot)
+    try:
+        print("Бот запустился")
+        await dp.start_polling(bot)
+    except:
+        print("Бот не запустился") 
+
 
 if __name__ == "__main__":
     asyncio.run(main())
-    
--- telebott.py
+++ telebott.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 from id import name
 
 TOKEN = name
@@ -6,14 +7,29 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(Commands = ["start"])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я простой бот на telebot")
+    keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    info = types.KeyboardButton("Инфо")
+    stat = types.KeyboardButton("Статистика")
+    keyboard_markup.add(info, stat)
+    bot.send_message(message, "Привет! Я простой бот на telebot")
 
 @bot.message_handler(func=lambda message:True)
 def echo_all(message):
-    bot.reply_to(message, f"Ты написал {message.text}")
-
+    if message.text == "Инфо":
+        bot.send_message(message, f"Ты нажал кнопку 'Инфо'")
+    elif message.text == "Статистика":
+        bot.send_message(message, f"Ты нажал кнопку 'Статистика'")
+        # bot.send_sticker(message, ':love:')
+    else:
+        bot.send_message(message, f"Ты написал {message.text}")
+        
 def main():
-    bot.polling(none_stop=True)
+    try:
+        print("Бот запустился")
+        bot.polling(none_stop=True)        
+    except:
+        print("Бот не запустился")
+        
 
 if __name__ == "__main__":
     main()
